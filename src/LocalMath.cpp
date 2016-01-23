006fa2b0f059266314dcaadf5f93ed13492b7e96
greg mori <gvmori@gmail.com>
2016-01-23 00:44:52 -0800

    moving math to separate namespace, beginning of player movement changes

---
+++ src/LocalMath.cpp
@@ -0,0 +1,15 @@
+#include "include/LocalMath.h"
+
+using namespace LocalMath;
+
+double LocalMath::DegToRad(double degrees){
+    return (degrees * pi() / 180);
+}
+    
+double LocalMath::RadToDeg(double degrees){
+    return (degrees * 180 / pi());
+}
+
+constexpr double LocalMath::pi() { 
+    return acos(-1); 
+}
---
+++ src/include/LocalMath.h
@@ -0,0 +1,13 @@
+#ifndef _LOCALMATH_H_
+    #define _LOCALMATH_H_
+
+#include <cmath>
+#include <algorithm>
+
+namespace LocalMath {
+    double RadToDeg(double degrees);
+    double DegToRad(double degrees);
+    constexpr double pi();
+}
+
+#endif
